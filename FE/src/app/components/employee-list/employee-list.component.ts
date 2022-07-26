271cf3d641e4f2a0e02a45df955ef07b01ebe7f5
Kathryn Cohen <kcohen@xpanxion.com>
2022-07-26 06:32:11 -0400

    Create EmployeeList Class

--- FE/src/app/components/employee-list/employee-list.component.ts
+++ FE/src/app/components/employee-list/employee-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'src/app/model/employee';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor() { }
+  employees!: Employee[];
+
+  constructor(private employeeService : EmployeeService, private router : Router) { }
 
   ngOnInit(): void {
+    this.getEmployees();
   }
 
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {this.employees = data});
+  }
+
+  employeeDetails(id:number){
+    this.router.navigate(['employee-details', id]);
+  }
+
+  updateEmployee(id:number){
+    this.router.navigate(['update-employee', id]);
+  }
+
+  deleteEmployee(id:number){
+    this.employeeService.deleteEmployee(id).subscribe(data => {
+      console.log(data);
+      this.getEmployees();
+    });
+  }
 }
