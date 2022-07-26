a8d5e8767c8bcf35d9fff412e206a4cd16a55be8
Kathryn Cohen <kcohen@xpanxion.com>
2022-07-26 09:43:38 -0400

    Component structure in place

--- FE/src/app/components/employee-details/employee-details.component.ts
+++ FE/src/app/components/employee-details/employee-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from 'src/app/model/Employee';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-employee-details',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailsComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  employee!: Employee;
+
+  constructor(private route: ActivatedRoute, private router: Router, private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.employee = new Employee();
+    this.employeeService.getEmployeeById(this.id).subscribe( data => {
+      this.employee = data;
+    }); 
+  }
+
+  updateEmployee(id:number){
+    this.router.navigate(['update-employee', id]);
+  }
+
+  backToList(){
+    this.router.navigate(['employees']);
   }
 
 }
